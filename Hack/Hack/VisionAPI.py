f8315132c32617e294747a7c5cd4bca3c4e71b06
Alex Jiang <alexjiang04@gmail.com>
2023-02-11 06:35:57 -0500

    start DB with good cloud

--- Hack/Hack/VisionAPI.py
+++ Hack/Hack/VisionAPI.py
@@ -3,18 +3,29 @@ import io
 from google.cloud import vision
 from google.cloud.vision_v1 import types
 
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
 
 client = vision.ImageAnnotatorClient()
-print(client)
 
-Filename = "Stop_sign_light_red.svg.png"
+Filename = "receipt_example_2.svg.png"
 Dir = "Data"
 
-with io.open(os.path.join(Dir,Filename),'rb' ) as imageFile:
+with io.open(os.path.join(Dir, Filename), 'rb') as imageFile:
     content = imageFile.read()
 
 image = types.Image(content=content)
 response = client.text_detection(image=image)
 texts = response.text_annotations
-print(texts)
+
+# for text in texts:
+    # if text.description.isalpha(): 
+fullText = '\n"{}"'.format(texts[0].description).split("\n")
+foodList = []
+
+for word in fullText:
+    temp = word.replace(" ","")
+    if temp.isalpha() and len(word)>2:
+        foodList.append(word)
+
+print(foodList)
