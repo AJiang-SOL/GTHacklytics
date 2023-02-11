2f5456698b08647a9dbe4e442a9412ada803ff35
Alex Jiang <alexjiang04@gmail.com>
2023-02-11 02:20:01 -0500

    got vision to work

--- Hack/Hack/VisionAPI.py
+++ Hack/Hack/VisionAPI.py
@@ -7,3 +7,14 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
 
 client = vision.ImageAnnotatorClient()
 print(client)
+
+Filename = "Stop_sign_light_red.svg.png"
+Dir = "Data"
+
+with io.open(os.path.join(Dir,Filename),'rb' ) as imageFile:
+    content = imageFile.read()
+
+image = types.Image(content=content)
+response = client.text_detection(image=image)
+texts = response.text_annotations
+print(texts)
