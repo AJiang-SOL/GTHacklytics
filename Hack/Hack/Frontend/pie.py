4a90dd30384a725baf115a4c2d59d144f7265841
Nykral <tnhxngo@gmail.com>
2023-02-12 07:10:59 -0500

    hbnvnv

--- Hack/Hack/Frontend/pie.py
+++ Hack/Hack/Frontend/pie.py
@@ -5,46 +5,56 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-import plotly.express as px
+import plotly.express as const
 import plotly
 
+const pieChartData = [
+    {
+    values: [],
+    labels: [],
+    type: "pie",
+    },
+];
 
-df = pd.read_excel('Statistics.xlsx')
+const
 
-app = dash.Dash(__name__)
-app.layout = html.Div([
-        html.Div([
-        html.Label(['Filter by: ']),
-        dcc.Dropdown(
-            id='dropdwn',
-            options=[{'label': 'Past month', 'value': 'Pi month'},
-                     {'label': 'Past 2 months', 'value': 'Pi 2 months'}],
-            value='Pi month',
-            multi=False,
-            clearable=False,
-            style={"width":"50%"},
-        ),
-    ]),
-]),
 
-html.Div([
-    dcc.Graph(id='graph')
-]),
+# df = pd.read_excel('Statistics.xlsx')
 
-@app.callback(
-    Output(component_id='graph',component_property='figure')
-    [Input(component_id='dropdwn',component_property='value')]
-)
+# app = dash.Dash(__name__)
+# app.layout = html.Div([
+#         html.Div([
+#         html.Label(['Filter by: ']),
+#         dcc.Dropdown(
+#             id='dropdwn',
+#             options=[{'label': 'Past month', 'value': 'Pi month'},
+#                      {'label': 'Past 2 months', 'value': 'Pi 2 months'}],
+#             value='Pi month',
+#             multi=False,
+#             clearable=False,
+#             style={"width":"50%"},
+#         ),
+#     ]),
+# ]),
 
-def update_graph(dropdwn):
-    dff = df
-    app=px.pie(
-        data_frame=dff,
-        names=dropdwn,
-        hole=.3,
-    )
-    return (app)
+# html.Div([
+#     dcc.Graph(id='graph')
+# ]),
 
-plotly.offline.plot(app, filename="pichart.html")
+# @app.callback(
+#     Output(component_id='graph',component_property='figure')
+#     [Input(component_id='dropdwn',component_property='value')]
+# )
 
-app.run_server(debug=True)
+# def update_graph(dropdwn):
+#     dff = df
+#     app=px.pie(
+#         data_frame=dff,
+#         names=dropdwn,
+#         hole=.3,
+#     )
+#     return (app)
+
+# plotly.offline.plot(app, filename="pichart.html")
+
+# app.run_server(debug=True)
